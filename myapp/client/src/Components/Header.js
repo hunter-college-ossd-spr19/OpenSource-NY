733612895f42e695ed6a0d82793ff73a00efd442
mxmsunny <xiangminmo6121@gmail.com>
2019-05-08 04:56:14 -0400

    complete makeover of how the website looks

--- myapp/client/src/Components/Header.js
+++ myapp/client/src/Components/Header.js
@@ -4,10 +4,20 @@ import Logo from '../Images/icon.png'
 
 function Header(){
     return(
-        <div className="header">
+        <div>
+            <div className="navBG">
+                <div className="logoStyle"><b>OSNY</b> App</div>
+                <div className="optionStyle">
+                    <a href="https://github.com/opensource-ny/OpenSource-NY">GitHub</a>
+                    <a href="https://github.com/opensource-ny/OpenSource-NY/blob/master/README.md">About</a>
+                </div>
+                <img className="logo" src={Logo} alt="Icon"/>
+            </div>
+        </div>
+        /*<div className="header">
             <p className="title">OpenSource NY App</p>
             <img src={Logo} alt="Icon"/>
-        </div>
+        </div>*/
     )
 }
 
