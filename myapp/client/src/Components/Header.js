37fc762eefec97b976f3181951bf5865fe3ef731
mxmsunny <xiangminmo6121@gmail.com>
2019-05-10 01:55:46 -0400

    made the logo and app name refresh page on click

--- myapp/client/src/Components/Header.js
+++ myapp/client/src/Components/Header.js
@@ -6,12 +6,16 @@ function Header(){
     return(
         <div>
             <div className="navBG">
-                <div className="logoStyle"><b>OSNY</b> App</div>
+                <div className="logoStyle">
+                    <a href="."><b>OSNY</b> App</a>
+                </div>
                 <div className="optionStyle">
                     <a href="https://github.com/opensource-ny/OpenSource-NY">GitHub</a>
                     <a href="https://github.com/opensource-ny/OpenSource-NY/blob/master/README.md">About</a>
                 </div>
-                <img className="logo" src={Logo} alt="Icon"/>
+                <a href=".">
+                    <img className="logo" src={Logo} alt="Icon"/>
+                </a>
             </div>
         </div>
         /*<div className="header">
