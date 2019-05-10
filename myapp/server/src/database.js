4f2dcc846c146012dbfeec035955a7e4715b8206
Undid-Iridium <francis.x.irizarry@gmail.com>
2019-05-10 12:34:35 -0400

    Some documentation

--- myapp/server/src/database.js
+++ myapp/server/src/database.js
@@ -4,13 +4,13 @@ function typeOf(obj) {
   return {}.toString.call(obj).split(' ')[1].slice(0, -1).toLowerCase();
 }
 
-const cors = require('cors')
+const cors = require('cors') //Future proofing to prevent cross origin issue
 let globalVariable = { db: null };
 
 function initAssessmentRoutes(options) {
     globalVariable = options;
 }    
-
+//These are used to keep global variable to connect to database.
 
 
 module.exports = {
@@ -18,6 +18,10 @@ module.exports = {
     initAssessmentRoutes,
 };
 
+//Exporting router and database
+
+
+//Route to get data from the database, searchs by username and repository and then sends it back as json
 router.get('/db', (req, res) => {
 
     var usernameV = req.query.username;
@@ -39,6 +43,11 @@ router.get('/db', (req, res) => {
 
 });
 
+
+
+//Post route to add a user to the database based on repository. It first checks if the user + repository exists, if either does not exist then it creates them based on the provided information
+//If they do exist then we go to the else condition where we iterate every single pr in this repository by this user, if they have done nothing new it does not change anything; however, (66-83)
+//if they have added new issues/prs then it is automatically added to a temp copy of the prs, and the user repository count is updated (line 89);
 router.post('/addToDB', cors(),(req, res) => {
   const note = { username : req.body.User , repoistory : req.body.Repository , pullRequests : JSON.stringify(JSON.parse(req.body.Repos), null, 2) };
   console.log(typeOf(note.username));
@@ -84,6 +93,8 @@ router.post('/addToDB', cors(),(req, res) => {
   
 });
 
+
+//Searches by username to remove the user from the database, nothing extreme. 
 router.delete('/removeFromDB/:username', (req, res) => {
   const username = req.params.username;
   try{
